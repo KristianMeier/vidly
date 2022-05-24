c60434a85bf0dbb8af03cd11dd5ee7a9f2cb83d2
Kristian Meier <kristianmeier@Kristians-MacBook-Air.local>
2022-05-24 16:28:16 +0200

    Chap 9, mosh delete fixed

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
+    count: 0,
     tags: ["tag1", "tag2", "tag3"],
   };
 
@@ -18,13 +18,35 @@ class Counter extends Component {
     );
   }
 
+  handleIncrement() {
+    console.group("Increment Clicked");
+  }
+
   render() {
     return (
       <div>
         {this.state.tags.length === 0 && "New tag"}
         {this.renderTags()}
+        <span className={this.getBudgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn bton-secondary btn-sm"
+        >
+          {"Icrement"}
+        </button>
       </div>
     );
+  }
+
+  getBudgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state; // Dette er objek dekonstruering. Man napper count ud af objektet. SUper elegenat.
+    return count === 0 ? "Zero" : count;
   }
 }
 
